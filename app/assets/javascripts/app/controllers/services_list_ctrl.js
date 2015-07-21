b135f33805383b54fa91a4a0861e0fe87d838980
Ozifrankly Silva <ozifrankly@gmail.com>
2015-07-20 22:46:22 -0300

    Fix Service

--- app/assets/javascripts/app/controllers/services_list_ctrl.js
+++ app/assets/javascripts/app/controllers/services_list_ctrl.js
@@ -22,13 +22,14 @@ angular.module("IREC").controller("ServiceListCtrl",["$scope", "$http", function
     }
   }
 
-  $scope.edit = function(name, id){
+  $scope.edit = function(name, service_type, id){
     $scope.name = name;
+    $scope.service_type_id = ""+service_type+"";
     $scope.id = id;
   }
 
   var saveservices = function(){
-    $http.post("services.json", { service: { name: $scope.name }} )
+    $http.post("services.json", { service: { name: $scope.name, service_type_id: $scope.service_type_id }} )
   .success(function(data){
     $scope.page.services.unshift(data);
     $scope.name = "";
@@ -36,16 +37,19 @@ angular.module("IREC").controller("ServiceListCtrl",["$scope", "$http", function
   }
 
   var updateservices = function(){
-    $http.put("services/"+$scope.id+".json", { service: { name: $scope.name }} )
+    $http.put("services/"+$scope.id+".json", { service: { name: $scope.name, service_type_id: $scope.service_type_id }} )
   .success(function(data){
     var services = $scope.page.services;
     for(index in $scope.page.services){
        if(services[index].id == $scope.id){
           services[index].name = $scope.name;
+          services[index].service_type.id = data.service_type.id;
+          services[index].service_type.name = data.service_type.name;
           break;
       }
     }  
     $scope.name = "";
+    $scope.service_type_id = '0';
     $scope.id = 0;
   });
   }
