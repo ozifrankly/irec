54393a726c2a7e501aa00f0af7f58ff442a4aa8b
Ozifrankly Silva <ozifrankly@gmail.com>
2015-07-23 23:09:32 -0300

    Fix minor

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,6 +14,7 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.create(student_params)
     if @student.errors.empty?
       redirect_to @student 
     else
