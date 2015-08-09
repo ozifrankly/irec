3bf24550921db5317be2c8b55f3ec2740a34c01f
Ozifrankly Silva <ozifrankly@gmail.com>
2015-08-09 15:11:29 -0300

    Fix fonts path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,5 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.paths << Rails.root.join("vendor","assets","bower_components", "font")
+Rails.application.config.assets.paths << Rails.root.join("vendor","assets","bower_components", "fonts")
 Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf)
